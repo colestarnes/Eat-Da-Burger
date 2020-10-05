e4732a0412b48b2786001625fc1eab44a2292dab
Cole Starnes <colestarnes1@gmail.com>
2020-10-05 16:24:14 -0500

    commitment

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require("express"); 
+
+var PORT = process.env.PORT || 8080; 
+
+var app = express(); 
+
+app.use(express.urlencoded("public"));
+ 
+require("dotenv").config();
+//parse application body as json
+app.use(express.urlencoded({ extended: true})); 
+app.use(express.json()); 
+
+//set handlebars
+var exphbs = require("express-handlebars"); 
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" })); 
+app.set("view engine", "handlebars"); 
+// import routes and give the server access to them
+var routes = require("./controllers/burgers_controller.js") 
+
+app.use(routes); 
+
+app.listen(PORT, function() {
+    console.log("Server listening on http://localhost:" + PORT);
+});
